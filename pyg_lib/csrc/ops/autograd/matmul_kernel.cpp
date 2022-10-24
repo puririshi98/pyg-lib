02bdef2729882ba80ccc574665708fbe7136fe04
riship <riship@nvidia.com>
2022-10-24 13:42:08 -0700

    autograd for grouped matmul

--- pyg_lib/csrc/ops/autograd/matmul_kernel.cpp
+++ pyg_lib/csrc/ops/autograd/matmul_kernel.cpp
@@ -112,8 +112,8 @@ class SegmentMatmul : public torch::autograd::Function<SegmentMatmul> {
 };
 
 
-std::vector<at::Tensor> grouped_matmul_autograd(const variable_list input,
-                                       const variable_list other) {
+std::vector<at::Tensor> grouped_matmul_autograd(const at::TensorList input,
+                                       const at::TensorList other) {
   return GroupedMatmul::apply(input, other);
 }
 
