1a06b676dfa3a1c5cdb589d6d4ee3710ddfb8010
riship <riship@nvidia.com>
2022-10-25 10:11:25 -0700

    autograd for grouped matmul

--- pyg_lib/ops/__init__.py
+++ pyg_lib/ops/__init__.py
@@ -17,7 +17,7 @@ class GroupedMatmul(torch.autograd.Function):
         return outs
 
     @staticmethod
-    def backward(ctx, *outs_grad):
+    def backward(ctx, outs_grad: List[Tensor]):
         print('inside backward')
         args = ctx.saved_tensors
         input_len = len(outs_grad)
